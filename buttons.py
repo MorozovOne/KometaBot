90b2ea75a13f827b479df80911af8edcc801ba62
MidN1ght <114833341+MorozovOne@users.noreply.github.com>
2024-12-15 00:07:27 +0300

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,15 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def get_instruction_buttons():
+    markup = InlineKeyboardMarkup()
+    #markup.add(InlineKeyboardButton("Инструкция для IOS/MacOS", callback_data="instruction_ios"))
+    #markup.add(InlineKeyboardButton("Инструкция для Android", callback_data="instruction_android"))
+    return markup
+
+def get_main_menu():
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("Создать аккаунт", callback_data="generate_key"))
+    #markup.add(InlineKeyboardButton("Удалить ключ", callback_data="delete_key"))
+    #markup.add(InlineKeyboardButton("Инструкция", callback_data="show_instructions"))
+    markup.add(InlineKeyboardButton("Скачать приложение", callback_data="show_app"))
+    return markup
