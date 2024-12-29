a7fd8df115263f0d15d9e0b63c0bf42c504cc5b6
MidN1ght <114833341+MorozovOne@users.noreply.github.com>
2024-12-30 00:13:08 +0300

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -2,14 +2,14 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_instruction_buttons():
     markup = InlineKeyboardMarkup()
-    #markup.add(InlineKeyboardButton("Инструкция для IOS/MacOS", callback_data="instruction_ios"))
-    #markup.add(InlineKeyboardButton("Инструкция для Android", callback_data="instruction_android"))
+    markup.add(InlineKeyboardButton("Инструкция для IOS/MacOS", callback_data="instruction_ios"))
+    markup.add(InlineKeyboardButton("Инструкция для Android", callback_data="instruction_android"))
     return markup
 
 def get_main_menu():
     markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("Создать аккаунт", callback_data="generate_key"))
-    #markup.add(InlineKeyboardButton("Удалить ключ", callback_data="delete_key"))
-    #markup.add(InlineKeyboardButton("Инструкция", callback_data="show_instructions"))
+    markup.add(InlineKeyboardButton("Создать ключ / Аккаунт", callback_data="generate_key"))
+    markup.add(InlineKeyboardButton("Удалить ключ / Аккаунт", callback_data="delete_key"))
+    markup.add(InlineKeyboardButton("Инструкция", callback_data="show_instructions"))
     markup.add(InlineKeyboardButton("Скачать приложение", callback_data="show_app"))
     return markup
